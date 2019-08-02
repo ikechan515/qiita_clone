aff040e93d6434c39d5168077895c1f2d2b8bd81
ikedayutaka <YI@mac.local>
2019-08-02 21:44:14 +0900

    create register page

--- app/javascript/packs/app.ts
+++ app/javascript/packs/app.ts
@@ -2,6 +2,8 @@ import Vue from "vue";
 import Router from "./router/router";
 import Header from "./container/Header.vue";
 import Vuetify from "vuetify";
+import "vuetify/dist/vuetify.min.css";
+// import "@fortawesome/fontawesome-free/css/all.css";
 
 Vue.use(Vuetify);
 
