293257b3e7beb7d166901e7ed0b8a8c0351423cf
ikedayutaka <YI@mac.local>
2019-08-02 15:43:05 +0900

    Add Navigation bar by vuetify

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,7 +1,7 @@
-const { environment } = require('@rails/webpacker');
-const typescript =  require('./loaders/typescript');
-const { VueLoaderPlugin } = require('vue-loader');
-const vue = require('./loaders/vue');
+const { environment } = require("@rails/webpacker");
+const typescript = require("./loaders/typescript");
+const { VueLoaderPlugin } = require("vue-loader");
+const vue = require("./loaders/vue");
 
 environment.plugins.prepend("VueLoaderPlugin", new VueLoaderPlugin());
 environment.loaders.prepend("vue", vue);
