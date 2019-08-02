d3d79ddb580a4542717a8f6f94eae8537eb27e8b
ikedayutaka <YI@mac.local>
2019-08-03 06:30:00 +0900

    Add articles draft function

--- spec/factories/articles.rb
+++ spec/factories/articles.rb
@@ -23,6 +23,15 @@ FactoryBot.define do
   factory :article do
     title { Faker::Lorem.word }
     body { Faker::Lorem.sentence }
+    status { :published }
     user
+  end
+
+  trait :draft do
+    status { :draft }
+  end
+
+  trait :published do
+    status { :published }
   end
 end
